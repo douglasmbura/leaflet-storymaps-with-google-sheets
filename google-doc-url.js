12dd2b35381e7859ea783afbfc5fd7e5dda5b121
douglasmbura <douglas.mbura@gmail.com>
2020-06-06 14:28:27 +0300

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1AO6XHL_0JafWZF4KEejkdDNqfuZWUk3SlNlQ6MjlRFM/';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1GIv1KUrRn0f3x9SQc8lsywKvpwwE9cByiIt4X5jRdOQ/edit#gid=0';
